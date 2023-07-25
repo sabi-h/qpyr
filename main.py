3aec56dfcd5d9c1458b4c0e771f9ed28d2e101eb
Sabih Hasan <sabih.hasan@choreograph.com>
2023-07-25 09:30:19 +0100

    add error correction

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from typing import Dict
+from typing import Dict, List
 import re
 
+from reedsolo import RSCodec, ReedSolomonError
 
-def _get_best_mode(data):
+
+VERSION_CAPACITIES_BY_ECC_MAPPING = {
+    "LOW": {
+        # verion: (total_capacity, data_capacity, ecc_capacity)
+        "1": (26, 19, 7),
+        "2": (44, 34, 10),
+        "3": (70, 55, 15),
+        "4": (100, 80, 20),
+        "5": (134, 108, 26),
+    }
+}
+
+def _validate_ecc_level(ecc_level: str) -> str:
+    supported_ecc_levels = VERSION_CAPACITIES_BY_ECC_MAPPING.keys()
+    if ecc_level not in supported_ecc_levels:
+        raise ValueError(f'Invalid ECC level. Must be one of {supported_ecc_levels}')
+    return ecc_level
+    
+
+def _get_best_mode(data: str) -> str:
     numeric_regex: re.Pattern = re.compile(r"[0-9]*")
     alphanumeric_regex: re.Pattern = re.compile(r"[A-Z0-9 $%*+./:-]*")
     is_byte = lambda data: all(ord(char) < 256 for char in data) # ISO-8859-1 characters
@@ -33,7 +53,7 @@ def _get_segment_character_count(data):
     return bin(len(data))[2:].zfill(8)
 
 
-def _get_segment_terimator():
+def _get_segment_terminator() -> str:
     return '0000'
 
 
@@ -41,54 +61,85 @@ def _get_segment(segments: list[str]):
     return ''.join(segments)
 
 
-def _get_best_version(segment: str):
-    version_capacity_mapping = {
-        # version: ecl_M_capacities
-        "1": 16,
-        "2": 28,
-        "3": 44,
-        "4": 64,
-        "5": 86,
-    }
-
-    number_of_codewords = len(segment) // 8
-    print("number of codewords:", number_of_codewords)
-
-    for version, capacity in version_capacity_mapping.items():
-            if number_of_codewords <= capacity:
-                print(version)
-                return version
+def _get_best_version(segment: str, ecc_level: str) -> str:
+    data_codewords = len(segment) // 8
+    for version, capacities in VERSION_CAPACITIES_BY_ECC_MAPPING[ecc_level].items():
+        data_capacity = capacities[1]
+        if data_codewords <= data_capacity:
+            return version
     raise ValueError('Data too long')
 
 
-def main(data: str):
+def _get_data_with_ecc_codewords(data: str, version: str, ecc_level: str):
+    number_of_ecc_codewords = VERSION_CAPACITIES_BY_ECC_MAPPING[ecc_level][version][2]
+    data_bytes = bytearray(data, encoding="utf-8")
+    rsc = RSCodec(nsym=number_of_ecc_codewords)
+    encoded_message = rsc.encode(data_bytes)
+    return encoded_message
+
+
+def _binary_str_to_hex(binary_str: str) -> str:
+    """Splits binary string into 8-bit chunks and converts each chunk to hex."""
+    result = []
+    binary_str_split: List[str] = [binary_str[i:i+8] for i in range(0, len(binary_str), 8)]
+    for binary_str in binary_str_split:
+        first_4_bits = hex(int(binary_str[:4], 2))[2:]
+        last_4_bits = hex(int(binary_str[4:], 2))[2:]
+        result.append(first_4_bits + last_4_bits)
+    return ' '.join(result)
+
+
+def _get_ecc_codewords(data: str, version: str, ecc_level: str):
+    pass
+
+
+def main(data: str, ecc_level: str = 'LOW'):
     """Create a QR code from data.
 
     Args:
         data (str): data to encode
-        error_correction_level (str, optional): can be 'L', 'M', 'Q' or 'H'. Defaults to 'L'.
     """
+    ecc_level = _validate_ecc_level(ecc_level)
+
     mode = _get_best_mode(data)
     print('Best mode:', mode)
 
     mode_segment = _get_segment_mode(mode)
     chr_count_segment = _get_segment_character_count(data)
     data_segment = _get_segment_data(data)
-    terimator_segment = _get_segment_terimator()
-    segment = _get_segment([mode_segment, chr_count_segment, data_segment, terimator_segment])
-    print('Data segment:', segment)
+    terminator_segment = _get_segment_terminator()
+    segment = _get_segment([mode_segment, chr_count_segment, data_segment, terminator_segment])
+    print('Data segment:', segment, "\nhex:", _binary_str_to_hex(segment))
 
-    version = _get_best_version(segment)
+    version = _get_best_version(segment, ecc_level)
     print('Best version:', version)
 
+    all_codewords = _get_data_with_ecc_codewords(data, version, ecc_level)
+    print('ECC codewords:', all_codewords) # "\nhex:", _binary_str_to_hex(all_codewords)
+
+    # segment_bytes = _to_bytes(segment)
+    # print('Segment bytes:', segment_bytes)
+
+    # hex_segment = _binary_to_hex(segment_bytes)
+    # print('Hex segment:', hex_segment)
+
+
+    # all_codewords_split = [all_codewords[i:i+8] for i in range(0, len(all_codewords), 8)]
+    # print('Codewords split:', all_codewords_split)
+
+    # for codeword in all_codewords_split:
+    #     hex_data = _binary_to_hex(all_codewords_split)
+    #     print('Hex data:', hex_data)
 
 
 if __name__ == '__main__':
     data = "Hello, world! 123"
+    # data = "Helloooo09daay"
     print('Data:', data)
 
     # tests
+    assert _validate_ecc_level('LOW') == 'LOW'
     assert _get_best_mode("Hello, world! 123") == 'byte'
-    assert _get_best_version("10101010"*43) == "3"
+    assert _get_best_version("10101010"*43, "LOW") == "3"
 
-    main(data) 
+    main(data, ecc_level='LOW') 
